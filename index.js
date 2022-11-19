8e76790857e16a47a75c41e40525b6f48d6c2c8f
Moringa School <lizdmukami@gmail.com>
2022-11-20 01:03:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-function addingEventListener() {
+// let input;
+// function addingEventListener() {
+//     input = document.getElementById('button');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addingEventListener('click', clickAlert);
+// }
+// addingEventListener();
+
+let input = document.getElementById('button');
+
+function addingEventListener()  {
+  alert('I was clicked!');
+input.addEventListener('input', addingEventListener);
 }
